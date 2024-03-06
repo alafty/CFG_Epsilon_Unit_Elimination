e25b566421e459585aa7a72fdc1c88f6742300fa
alafty <mohamedelalafty@yahoo.com>
2024-03-06 23:47:10 +0200

    Initial commit

---
+++ src/csen1002/main/task4/CfgEpsUnitElim.java
@@ -0,0 +1,47 @@
+package csen1002.main.task4;
+
+/**
+ * Write your info here
+ * 
+ * @name Mohamed Moustafa
+ * @id 49-3603
+ * @labNumber 21
+ */
+
+public class CfgEpsUnitElim {
+
+	/**
+	 * Constructs a Context Free Grammar
+	 * 
+	 * @param cfg A formatted string representation of the CFG. The string
+	 *             representation follows the one in the task description
+	 */
+	public CfgEpsUnitElim(String cfg) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @return Returns a formatted string representation of the CFG. The string
+	 *         representation follows the one in the task description
+	 */
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * Eliminates Epsilon Rules from the grammar
+	 */
+	public void eliminateEpsilonRules() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * Eliminates Unit Rules from the grammar
+	 */
+	public void eliminateUnitRules() {
+		// TODO Auto-generated method stub
+	}
+
+}
